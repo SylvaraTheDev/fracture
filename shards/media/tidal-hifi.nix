f3af7cbb83b34fbf684a38ee994da5049dd00939
SylvaraTheDev <wing@elyria.dev>
2026-02-12 10:01:45 +1000

    Fix.

--- shards/media/tidal-hifi.nix
+++ shards/media/tidal-hifi.nix
@@ -2,14 +2,17 @@
 
 let
   inherit (config.fracture.user) login;
-  tidal-hifi-wayland = pkgs.tidal-hifi.overrideAttrs (prev: {
-    installPhase = (prev.installPhase or "") + ''
+  tidal-hifi-wayland = pkgs.symlinkJoin {
+    name = "tidal-hifi";
+    paths = [ pkgs.tidal-hifi ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
       wrapProgram $out/bin/tidal-hifi \
         --add-flags "--ozone-platform=wayland" \
         --add-flags "--enable-features=UseOzonePlatform,WaylandWindowDecorations" \
         --add-flags "--enable-wayland-ime"
     '';
-  });
+  };
 in
 {
   home-manager.users.${login} = _: {
