54c5aa128537291a4edbbca01927a1898fab0128
SylvaraTheDev <wing@elyria.dev>
2026-02-23 11:23:54 +1000

    Fix.

--- shards/desktop/krita.nix
+++ shards/desktop/krita.nix
@@ -2,12 +2,25 @@
 
 let
   inherit (config.fracture.user) login;
+
+  krita-ai-diffusion = pkgs.fetchFromGitHub {
+    owner = "Acly";
+    repo = "krita-ai-diffusion";
+    rev = "v1.48.0";
+    hash = "sha256-YXOmf6rBYPInug4Da010BUhFx//F7od/ebCPovCVZlU=";
+  };
 in
 {
   home-manager.users.${login} = _: {
     home.packages = with pkgs; [
       krita
     ];
+
+    home.file = {
+      ".local/share/krita/pykrita/ai_diffusion".source = "${krita-ai-diffusion}/ai_diffusion";
+      ".local/share/krita/pykrita/ai_diffusion.desktop".source =
+        "${krita-ai-diffusion}/ai_diffusion.desktop";
+    };
 
     home.persistence."/persist".directories = [
       ".config/krita-scripter"
