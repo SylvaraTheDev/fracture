aa18aebdc57abdee5b244d0606e46248eda58bc2
SylvaraTheDev <wing@elyria.dev>
2026-03-03 12:03:01 +1000

    Updated Deluge ports.

--- shards/media/deluge.nix
+++ shards/media/deluge.nix
@@ -4,6 +4,11 @@ let
   inherit (config.fracture.user) login;
 in
 {
+  networking.firewall = {
+    allowedTCPPorts = [ 6881 ];
+    allowedUDPPorts = [ 6881 ];
+  };
+
   home-manager.users.${login} = _: {
     home.packages = with pkgs; [
       deluge
